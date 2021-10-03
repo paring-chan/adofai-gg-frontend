0869afc7534db3e42c84c73f9cc48042a9c80816
파링 <parangee9706@gmail.com>
2021-10-03 12:03:01 +0900

    fix: fix error page and add custom 404 route

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ErrorContainer } from './_error'
+
+const NotFound: React.FC = () => {
+  return <ErrorContainer>Page Not Found</ErrorContainer>
+}
+
+export default NotFound
--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,30 +1,15 @@
 import React from 'react'
-import { NextPageContext } from 'next'
 import styled from 'styled-components'
 
-const Container = styled.div`
+export const ErrorContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 30px;
   font-size: 40px;
 `
 
-const Error = ({ statusCode }: { statusCode: number }) => {
-  const getMessage = () => {
-    switch (statusCode) {
-      case 404:
-        return 'Page not found.'
-      default:
-        return 'Oops! An error occurred.'
-    }
-  }
-
-  return <Container>{getMessage()}</Container>
-}
-
-Error.getInitialProps = ({ res, err }: NextPageContext) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
+const Error = () => {
+  return <ErrorContainer>Oops! An error occurred.</ErrorContainer>
 }
 
 export default Error
